1600ccf3546d860eacd4df90d055dd4271e95053
Ikram ullah <ikramsalarzai1@gmail.com>
2023-12-13 21:47:11 +0500

    credential error

--- resources/views/client/auth/login.blade.php
+++ resources/views/client/auth/login.blade.php
@@ -1,11 +1,12 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
- 
+
     <title>Customer Login</title>
-    
+
     <link rel="stylesheet" href="{{ asset('assets/css/bootstrap.css') }}">
     <link rel="stylesheet" href="{{ asset('assets/vendors/bootstrap-icons/bootstrap-icons.css') }}">
     <link rel="stylesheet" href="{{ asset('assets/css/app.css') }}">
@@ -18,14 +19,25 @@
             <div class="col-lg-5 col-12">
                 <div id="auth-left" class="p-4">
                     <div class="auth-logo d-flex justify-content-center mb-4">
-                        <a href="/"><h1><u>Greenwich Landmark pictures</u></h1></a>
+                        <a href="/">
+                            <h1><u>Greenwich Landmark pictures</u></h1>
+                        </a>
                     </div>
                     <h2 class="text-center mb-4">Log in</h2>
+                    @if (session('errors'))
+                        <div class="alert alert-danger">
+                            @foreach (session('errors')->all() as $error)
+                                <p>{{ $error }}</p>
+                            @endforeach
+                        </div>
+                    @endif
                     <form method="POST" action="{{ route('customer.postlogin') }}">
                         @csrf
                         @method('POST')
                         <div class="mb-3">
-                            <input id="email" type="email" class="form-control @error('email') is-invalid @enderror" name="email" value="{{ old('email') }}" required autocomplete="email" autofocus placeholder="Email">
+                            <input id="email" type="email"
+                                class="form-control @error('email') is-invalid @enderror" name="email"
+                                value="{{ old('email') }}" required autocomplete="email" autofocus placeholder="Email">
                             @error('email')
                                 <span class="invalid-feedback" role="alert">
                                     <strong>{{ $message }}</strong>
@@ -33,7 +45,9 @@
                             @enderror
                         </div>
                         <div class="mb-3">
-                            <input id="password" type="password" class="form-control @error('password') is-invalid @enderror" name="password" required autocomplete="current-password" placeholder="Password">
+                            <input id="password" type="password"
+                                class="form-control @error('password') is-invalid @enderror" name="password" required
+                                autocomplete="current-password" placeholder="Password">
                             @error('password')
                                 <span class="invalid-feedback" role="alert">
                                     <strong>{{ $message }}</strong>
@@ -41,15 +55,17 @@
                             @enderror
                         </div>
                         <div class="mb-3 form-check">
-                            <input type="checkbox" class="form-check-input" name="remember" id="remember" {{ old('remember') ? 'checked' : '' }}>
+                            <input type="checkbox" class="form-check-input" name="remember" id="remember"
+                                {{ old('remember') ? 'checked' : '' }}>
                             <label class="form-check-label" for="remember">Remember me</label>
                         </div>
                         <button type="submit" class="btn btn-primary btn-block btn-lg shadow-lg">Log in</button>
                     </form>
-                         <div class="text-center mt-5 text-lg">
-                            <p class='text-gray-600'><a href="{{route('customer.signup')}}" class="font-bold">Register</a>.</p>
-                        </div>
-                 </div>
+                    <div class="text-center mt-5 text-lg">
+                        <p class='text-gray-600'><a href="{{ route('customer.signup') }}"
+                                class="font-bold">Register</a>.</p>
+                    </div>
+                </div>
             </div>
             <div class="col-lg-7 d-none d-lg-block">
                 <div id="auth-right-login">
